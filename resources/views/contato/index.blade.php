d3e2056c8c7c1c0575294f15beb9fbb4a8528dc7
Rinaldo <mundial.cel11@gmail.com>
2022-03-25 23:54:09 -0300

    criada visao inicio, contato

---
+++ resources/views/contato/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Visao da contato</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, asperiores, atque, autem, consequuntur, doloremque, dolorum, eaque, eius, eum, eveniet, explicabo, ipsa, ipsam, iste, itaque, laboriosam, libero, magnam, molestiae, nam, natus, neque, nihil, nostrum, nulla, odio, officia, omnis, perspiciatis, porro, quae, quam, quas, quis, quisquam, quod, quos, ratione, repellendus, rerum, sapiente, sed, sequi, sint, sit, sunt, suscipit, tempora, temporibus, tenetur, totam, ut, vel, veritatis, vero, voluptas, voluptatem.
+    </p>
+</body>
+</html>
