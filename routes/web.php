900429beaaf024bd3422f63e0c118737560419da
Rinaldo <mundial.cel11@gmail.com>
2022-03-26 00:22:03 -0300

    criando rotas com parametro inicio/{id}/{nome} e etc com varios parametro

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,17 @@ Route::get('/', [App\Http\Controllers\inicioController::class, 'index']);
 Route::get('/principal', [App\Http\Controllers\PrincipalController::class, 'index']);
 Route::get('/contato', [App\Http\Controllers\ContatolController::class, 'index']);
 Route::get('/inicio', [App\Http\Controllers\inicioController::class, 'index']);
+// se quise com parametro opcinais inicio/{id}/{nome?}/{sobrenome?}/{idade?}/{sexo?}
+Route::get('/inicio/{id}/{nome?}/{sobrenome?}/{idade?}/{sexo?}',
+   function($id, $nome = null, $sobrenome = null, $idade = null, $sexo = null){
+    return "ID: {$id}<br>Nome: {$nome}<br>Sobrenome: {$sobrenome}<br>Idade: {$idade}<br>Sexo: {$sexo}";
+   }
 
-Route::get('inicio/{id}', function (string $id) {
-    return "O id corresponde a : {$id}";
-});
+
+);
+// Route::get('inicio/{id}', function (string $id) {
+//     return "O id corresponde a : {$id}";
+// });
 
 
 
