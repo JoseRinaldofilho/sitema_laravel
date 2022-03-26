bc6d8903763b92eeb7b4ad5d8cc6d4d13939ce9c
Rinaldo <mundial.cel11@gmail.com>
2022-03-25 23:53:59 -0300

    criada visao inicio, contato

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [App\Http\Controllers\inicioController::class, 'index']);
 Route::get('/principal', [App\Http\Controllers\PrincipalController::class, 'index']);
+Route::get('/contato', [App\Http\Controllers\ContatolController::class, 'index']);
+Route::get('/inicio', [App\Http\Controllers\inicioController::class, 'index']);
+
